2fc08feda96bdfe63aa887ce62e27ae087ebf1d6
Kristopher Wuollett <kristopher@wuollett.net>
2012-06-09 18:26:20 -0400

    Added multi-module Maven project for a sample chat system closes #2

---
+++ kriswuollett-sandbox-chat/kriswuollett-sandbox-chat-server/src/main/java/kriswuollett/sandbox/chat/server/App.java
@@ -0,0 +1,13 @@
+package kriswuollett.sandbox.chat.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
