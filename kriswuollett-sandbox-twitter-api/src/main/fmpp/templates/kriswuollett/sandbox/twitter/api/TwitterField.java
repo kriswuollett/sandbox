9d2ded618e03ecec5230a6a5bddcce8f6d6c94ef
Kristopher Wuollett <kristopher@wuollett.net>
2012-06-19 06:58:45 -0400

    Added some sample documentation for Twitter fields for #6.  Need to remove try catch soon for unknown field.

--- kriswuollett-sandbox-twitter-api/src/main/fmpp/templates/kriswuollett/sandbox/twitter/api/TwitterField.java
+++ kriswuollett-sandbox-twitter-api/src/main/fmpp/templates/kriswuollett/sandbox/twitter/api/TwitterField.java
@@ -3,10 +3,14 @@ package kriswuollett.sandbox.twitter.api;
 public enum TwitterField
 {
 <#list twitterFields as field>
+    /** ${field.description} */ 
     ${field.name?upper_case},
+
 </#list>
+    /** An unrecognized field  */
     __UNKNOWN__;
-    
+
+    // TODO log warning about first time seeing unrecognized field and use own map to avoid try catch    
     public static TwitterField lookup( final String name )
     {
     	try
