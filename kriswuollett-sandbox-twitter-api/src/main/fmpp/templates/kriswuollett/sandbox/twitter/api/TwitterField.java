620746278ee6ece7b6e14937bcb453da8796546a
Kristopher Wuollett <kristopher@wuollett.net>
2012-12-01 21:36:35 -0800

    Updated other licenses to BSD-3-Clause.

--- kriswuollett-sandbox-twitter-api/src/main/fmpp/templates/kriswuollett/sandbox/twitter/api/TwitterField.java
+++ kriswuollett-sandbox-twitter-api/src/main/fmpp/templates/kriswuollett/sandbox/twitter/api/TwitterField.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2012, Kristopher Wuollett
+ * All rights reserved.
+ *
+ * This file is part of kriswuollett/twitter-api.
+ *
+ * kriswuollett/twitter-api is free software: you can redistribute it and/or modify
+ * it under the terms of the BSD 3-Clause License as written in the COPYING
+ * file.
+ */
 package kriswuollett.sandbox.twitter.api;
 
 public enum TwitterField
--- kriswuollett-sandbox-twitter-api/src/main/java/kriswuollett/sandbox/twitter/api/beans/TweetBean.java
+++ kriswuollett-sandbox-twitter-api/src/main/java/kriswuollett/sandbox/twitter/api/beans/TweetBean.java
@@ -1,5 +1,12 @@
-/**
- * 
+/*
+ * Copyright (c) 2012, Kristopher Wuollett
+ * All rights reserved.
+ *
+ * This file is part of kriswuollett/twitter-api.
+ *
+ * kriswuollett/twitter-api is free software: you can redistribute it and/or modify
+ * it under the terms of the BSD 3-Clause License as written in the COPYING
+ * file.
  */
 package kriswuollett.sandbox.twitter.api.beans;
 
